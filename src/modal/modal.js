ea9f9bed8cb72f02263de2cde7e1bc9f9490e154
Olivier Nizet <onizet@me.com>
2016-07-06 14:17:50 +0200

    Focus the first found control with autofocus attribute or fallback to the modal window itself.

--- src/modal/modal.js
+++ src/modal/modal.js
@@ -294,6 +294,9 @@ angular.module('mm.foundation.modal', ['mm.foundation.mediaQueries'])
             // This allows for scrolling
             options.scope.$watch(() => modalDomEl[0].offsetHeight, resizeHandler);
 
+			const focusedElem = (modalDomEl[0].querySelector('[autofocus]') || modalDomEl[0]);
+			focusedElem.focus();
+
             return $q.all(promises);
         });
     };
